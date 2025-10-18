f74634684bc4b38e5f0a24ad09f45aa0b5e27013
Andrew Panimdim <baelmeans74@gmail.com>
2025-10-19 02:50:40 +0800

    a

---
+++ du.js
@@ -0,0 +1,32 @@
+const minime = document.getElementById("minime");
+let pos = 10;
+let stationary_status = true;
+
+document.addEventListener("keydown", (event)=>{
+    if(event.key === "A" || event.key === "a"){
+        pos -= 2;
+        minime.style.left = pos + "%";
+        minime.src = "character/runningleft.gif";
+        stationary_status = false;
+        if(pos <= 2){
+            pos = 2;
+        }
+    }
+    else if(event.key === "D" || event.key === "d"){
+        pos += 2;
+        minime.style.left = pos + "%";
+        minime.src = "character/runningright.gif";
+        stationary_status = false;
+        if(pos >= 86){
+            pos = 86;
+            window.location.href = "https://www.facebook.com";
+        }
+    }
+});
+
+document.addEventListener("keyup", (event)=>{
+    if(['a','d','A','D'].includes(event.key)){
+        stationary_status = true;
+        minime.src = "character/standing.gif";
+    }
+});
