1627c8970f125ffc878bbd4645070fc739d4970d
Andrew Panimdim <baelmeans74@gmail.com>
2025-10-19 18:08:51 +0800

    a

---
+++ js/proj.js
@@ -0,0 +1,32 @@
+const minime = document.getElementById("minime");
+let pos = 40;
+let stationary_status = true;
+
+document.addEventListener("keydown", (event)=>{
+    if(event.key === "A" || event.key === "a"){
+        pos -= 2;
+        minime.style.left = pos + "%";
+        minime.src = "character/runningleft.gif";
+        stationary_status = false;
+        if(pos <= 2){
+            pos = 2;
+            window.location.href = "https://andrewpanimdim.github.io/gameportfolio/index.html";
+        }
+    }
+    else if(event.key === "D" || event.key === "d"){
+        pos += 2;
+        minime.style.left = pos + "%";
+        minime.src = "character/runningright.gif";
+        stationary_status = false;
+        if(pos >= 86){
+            pos = 86;
+        }
+    }
+});
+
+document.addEventListener("keyup", (event)=>{
+    if(['a','d','A','D'].includes(event.key)){
+        stationary_status = true;
+        minime.src = "character/standing.gif";
+    }
+});
