e0fc6cae364188d4eb02c1c9eb6996d6e480a6aa
Andrew Panimdim <baelmeans74@gmail.com>
2025-10-28 01:27:54 +0800

    ey

--- js/du.js
+++ js/du.js
@@ -1,6 +1,26 @@
 const minime = document.getElementById("minime");
 let pos = 40;
+let posx = 0
 let stationary_status = true;
+
+const keycontainer = document.getElementById("keycontainer");
+const akey = document.getElementById("a");
+const dkey = document.getElementById("d");
+const bio = document.getElementById("bio");
+setInterval(()=>{
+    akey.style.animation = "bouncing 1.5s ease-in-out infinite";
+    dkey.style.animation = "bouncing 2s ease-in-out infinite";
+},4000);
+
+
+
+
+document.addEventListener("keydown",(event)=>{
+    if(event.key == "A" || event.key == "a" || event.key == "D" || event.key == "d"){
+        keycontainer.style.animation = "fadeout 1s ease-in-out forwards";
+    }
+});
+
 
 document.addEventListener("keydown", (event)=>{
     if(event.key === "A" || event.key === "a"){
@@ -10,7 +30,27 @@ document.addEventListener("keydown", (event)=>{
         stationary_status = false;
         if(pos <= -2){
             pos = -2;
+            minime.style.top = 150 + "%";
+                window.scrollTo({
+                top: 1000,
+                left: 0,
+                behavior: 'smooth'
+                });
         }
+
+        if(pos > 70){
+            document.body.classList.add("lightmode");
+            document.body.style.transition = "all 0.5s ease-in-out";
+            document.body.style.backgroundColor = "white";
+            bio.style.color = "black";
+            }
+        if(pos < 70){
+                document.body.classList.remove("lightmode");
+                document.body.style.transition = "all 0.5s ease-in-out";
+                document.body.style.backgroundColor = "rgb(9, 9, 9)";
+                bio.style.color = "rgb(153, 151, 151)";
+            }
+        
     }
     else if(event.key === "D" || event.key === "d"){
         pos += 2;
@@ -19,10 +59,26 @@ document.addEventListener("keydown", (event)=>{
         stationary_status = false;
         if(pos >= 94){
             pos = 94;
-            window.location.href = "https://andrewpanimdim.github.io/gameportfolio/seemore.html";
+            window.location.href = "https://andrewpanimdim.github.io/gameportfolio/projects.html";
         }
+
+        if(pos > 70){
+            document.body.classList.add("lightmode");
+            document.body.style.transition = "all 0.5s ease-in-out";
+            document.body.style.backgroundColor = "white";
+            bio.style.color = "black";
+        }
+        if(pos < 70){
+            document.body.classList.remove("lightmode");
+            document.body.style.transition = "all 0.5s ease-in-out";
+            document.body.style.backgroundColor = "rgb(9, 9, 9)";
+            bio.style.color = "rgb(153, 151, 151)";        
+        }
+
     }
+
 });
+
 
 document.addEventListener("keyup", (event)=>{
     if(['a','d','A','D'].includes(event.key)){
@@ -30,3 +86,4 @@ document.addEventListener("keyup", (event)=>{
         minime.src = "character/standing.gif";
     }
 });
+
